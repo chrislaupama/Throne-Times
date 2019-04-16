d04b84106bbc3fd3ee6b15512049f6f7d2f80951
Chris <chrislaupama@icloud.com>
2019-04-16 17:00:22 +1200

    preshaped data in api, and use a .map in component

--- client/components/News.jsx
+++ client/components/News.jsx
@@ -3,66 +3,35 @@ import { getNews } from '../api/news'
 
 export default class News extends React.Component {
   state = {
-    news: {
-      article1: '',
-      article2: '',
-      article3: ''
-    }
+    news: []
   }
 
   componentDidMount() {
     getNews()
-    .then(res => {
-      this.setState({
-        news: {
-          article1: {
-            title: res.articles[0].title,
-            description: res.articles[0].description,
-            agent: res.articles[0].source.name,
-            content: res.articles[0].content
-          },
-          article2: {
-            title: res.articles[1].title,
-            description: res.articles[1].description,
-            agent: res.articles[1].source.name,
-            content: res.articles[1].content
-          },
-          article3: {
-            title: res.articles[2].title,
-            description: res.articles[2].description,
-            agent: res.articles[2].source.name,
-            content: res.articles[2].content
-          }
-        }
+      .then(res => {
+        this.setState({
+          news: res
+        })
       })
-    })
-    .catch(err => {
-      console.error('Error:', err)
-    })
+      .catch(err => {
+        console.error('Error:', err)
+      })
   }
 
   render() {
     return (
       <React.Fragment>
         <h2>Local News</h2>
-        <div>
-          <h5>{this.state.news.article1.title}</h5>
-          <em>{this.state.news.article1.description}</em>
-          <p>{this.state.news.article1.content}</p>
-          <p>- News sourced from {this.state.news.article1.agent}</p>
-        </div>
-        <div>
-          <h5>{this.state.news.article2.title}</h5>
-          <em>{this.state.news.article2.description}</em>
-          <p>{this.state.news.article2.content}</p>
-          <p>- News sourced from {this.state.news.article2.agent}</p>
-        </div>
-        <div>
-          <h5>{this.state.news.article3.title}</h5>
-          <em>{this.state.news.article3.description}</em>
-          <p>{this.state.news.article3.content}</p>
-          <p>- News sourced from {this.state.news.article3.agent}</p>
-        </div>
+        {this.state.news.map(article => {
+          return (
+            <div key={article.index}>
+              <h5>{article.title}</h5>
+              <em>{article.description}</em>
+              <p>{article.content}</p>
+              <p>- News sourced from {article.agent}</p>
+            </div>
+          )
+        })}
       </React.Fragment>
     )
   }
