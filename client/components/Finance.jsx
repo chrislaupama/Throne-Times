25eb2df061fdc35c577b42ad0d4648f82bde4870
Chris <chrislaupama@icloud.com>
2019-04-16 12:48:05 +1200

    converting api's to their own files

--- client/components/Finance.jsx
+++ client/components/Finance.jsx
@@ -1,63 +1,61 @@
 import React from 'react'
-import request from 'superagent'
+import {getFinance} from '../api/finance'
 
  export default class Finance extends React.Component {
   state = {
-    AT: '044cdf6ea4ae4a1d8cbdd309903569c1',
-    url: 'https://newsapi.org/v2/top-headlines?sources=financial-times&apiKey=',
-    news:{
+    news: {
       article1: '',
       article2: '',
       article3: '',
       article4: '',
       article5: ''
     }
-    
-  }
-  componentDidMount(){
-    this.callFinance()
   }
 
-  callFinance(){
-    request
-    .get(`${this.state.url}${this.state.AT}`)
-    .then( res => {
+  componentDidMount(){
+    getFinance()
+    .then(res => {
       this.setState({
         news: {
           article1: {
-            title: res.body.articles[0].title,
-            img: res.body.articles[0].urlToImage,
-            content: res.body.articles[0].content,
-            url: res.body.articles[0].url
+            title: res.articles[0].title,
+            img: res.articles[0].urlToImage,
+            content: res.articles[0].content,
+            url: res.articles[0].url
           }, 
           article2: {
-            title: res.body.articles[1].title,
-            img: res.body.articles[1].urlToImage,
-            content: res.body.articles[1].content,
-            url: res.body.articles[1].url
+            title: res.articles[1].title,
+            img: res.articles[1].urlToImage,
+            content: res.articles[1].content,
+            url: res.articles[1].url
           },
           article3: {
-            title: res.body.articles[2].title,
-            img: res.body.articles[2].urlToImage,
-            content: res.body.articles[2].content,
-            url: res.body.articles[2].url
+            title: res.articles[2].title,
+            img: res.articles[2].urlToImage,
+            content: res.articles[2].content,
+            url: res.articles[2].url
           },
           article4: {
-            title: res.body.articles[3].title,
-            img: res.body.articles[3].urlToImage,
-            content: res.body.articles[3].content,
-            url: res.body.articles[3].url
+            title: res.articles[3].title,
+            img: res.articles[3].urlToImage,
+            content: res.articles[3].content,
+            url: res.articles[3].url
           }, 
           article5: {
-            title: res.body.articles[4].title,
-            img: res.body.articles[4].urlToImage, 
-            content: res.body.articles[4].content,
-            url: res.body.articles[4].url
+            title: res.articles[4].title,
+            img: res.articles[4].urlToImage, 
+            content: res.articles[4].content,
+            url: res.articles[4].url
           }
         }
       })
     })
+    .catch(err => {
+      console.error('Error:', err)
+    })
   }
+  
+
     render() {
         return (
           <React.Fragment>
