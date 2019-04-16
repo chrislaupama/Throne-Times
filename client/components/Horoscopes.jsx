82b74f7c06d716d4f15520dfcf86bac278be35d7
Chris <chrislaupama@icloud.com>
2019-04-16 13:10:39 +1200

    convereted to use an api file

--- client/components/Horoscopes.jsx
+++ client/components/Horoscopes.jsx
@@ -1,31 +1,24 @@
 import React from 'react'
-import request from 'superagent'
+import {getHoroscopes} from '../api/horoscopes'
 
 export default class Horoscopes extends React.Component {
   state = {
     horoscope: {
-      URL: 'https://aztro.sameerkumar.website',
-      sign: '',
+      sign: this.props.sign,
       date: '',
       description: ''
     }
   }
-  componentDidMount() {
-    this.getHoroscope()
-  }
 
-  getHoroscope() {
-    const url =
-      'https://aztro.sameerkumar.website' +
-      '?sign=' +
-      this.props.sign +
-      '&day=today'
-    request.post(url).then(res => {
+  componentDidMount() {
+    const sign = this.props.sign
+    
+    getHoroscopes(sign)
+    .then(res => {
       this.setState({
         horoscope: {
-          sign: this.state.horoscope.sign,
-          date: res.body.date_range,
-          description: res.body.description
+          date: res.date_range,
+          description: res.description
         }
       })
     })
